5c8c455340fde29f1ad535f4e1427358c8cca2b5
KarolZSE <apple.szkola@gmail.com>
2025-12-02 11:34:00 +0000

    Made a AvaibleBlocks, the player will pick blocks to place from it, rebuild the way blocks are created

--- code.js
+++ code.js
@@ -1,11 +1,9 @@
 const Container = document.getElementById('GameContainer');
-let Count = 0;
 let Grid = [];
 for (let i = 0; i < 15; i++) {
     Grid[i] = [];
     for (let j = 0; j < 15; j++) {
         const block = document.createElement('div');
-        block.textContent = Count++;
         Grid[i][j] = block;       
         Container.appendChild(block);
     }
@@ -22,25 +20,41 @@ const positions = [
 for (let i = 0; i < 15; i++) {
     for (let j = 0; j < 15; j++) {
         if (Math.random() > 0.9) {
-            let GroupCount = 0;
-            let GroupElements = [];
-            for ([x, y] of positions) {
-                targetX = x + j;
-                targetY = y + i;
-                if (targetX >= 0 && targetY >= 0 && targetX <= 14 && targetY <= 14) {
-                    Grid[targetX][targetY].classList.add('Fill');
-                    GroupCount++;
-                    GroupElements.push(Grid[targetX][targetY]);
-                    Grid[targetX][targetY].addEventListener('click', () => {
-                        GroupCount--;
-                        if (GroupCount <= 0) {
-                            GroupElements.forEach(e => {
-                                e.classList.remove('Fill');
-                            });
-                        }
-                    }, { once: true});
-                }
-            }
+            SummonBlocks(Grid, i, j);
         }
     }
 }
+
+function SummonBlocks(array, i, j) {
+    let GroupCount = 0;
+    let GroupElements = [];
+    for ([x, y] of positions) {
+        targetX = x + j;
+        targetY = y + i;
+        if (targetX >= 0 && targetY >= 0 && targetX <= array.length - 1 && targetY <= array[0].length - 1) {
+            array[targetX][targetY].classList.add('Fill');
+            GroupCount++;
+            GroupElements.push(array[targetX][targetY]);
+            array[targetX][targetY].addEventListener('click', () => {
+                GroupCount--;
+                if (GroupCount <= 0) {
+                    GroupElements.forEach(e => {
+                        e.classList.remove('Fill');
+                    });
+                }
+            }, { once: true});
+        }
+    }
+}
+
+const Blocks = document.getElementById('A1');
+let B = [];
+for (let i = 0; i < 3; i++) {
+    B[i] = [];
+    for (let j = 0; j < 3; j++) {
+        const block = document.createElement('div');
+        B[i][j] = block;       
+        Blocks.appendChild(block);
+    }
+}
+SummonBlocks(B, 1, 1);
